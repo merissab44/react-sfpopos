d66025ddadc62bd7e913abc02b7253b7253b0254
Merissa Bridgeman <merissabridgeman@Merissas-MacBook-Pro-2.local>
2022-02-15 21:41:01 -0800

    header component built

---
+++ src/Title.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Title.css';
+function Title() {
+  return (
+    <>
+    <div className='Title'>
+      <h1>SFPOPOS</h1>
+    </div>
+    <div className='Title-Subtitle'>
+      <p>San Francisco Privately Owned Public Open Spaces</p>
+    </div>
+    </>
+  )
+}
+
+export default Title
