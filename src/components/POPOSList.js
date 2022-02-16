d604cc0e098ed17542b021d67bc0348c8b01621f
Merissa Bridgeman <merissabridgeman@Merissas-MacBook-Pro-2.local>
2022-02-15 23:45:43 -0800

    search

--- src/components/POPOSList.js
+++ src/components/POPOSList.js
@@ -1,24 +1,39 @@
 import POPOSSpace from "./POPOSSpace"
+import { useState } from "react"
 import './POPOSList.css'
-import data from '../sfpopos-data.json'
+import data from '../sfpopos-data'
 
 function POPOSList() {
 
+  const [ query, setQuery ] = useState('')
+
 //deconstruct the data array
-const spaces = data.map(( {title, address, images, hours}, i) => {
+const spaces = data.filter(({ features, title, address }) => {
+	}).map((obj, i) => { // remove i here
+			// Add id here!
+			const { id, title, address, images, hours, features } = obj
+		return (
+			<POPOSSpace
+				id={id} // use id here
+				key={`${title}-${id}`} // use id here
+        name={title}
+        address={address}
+				image={images[0]}
+				hours={hours}
+        features={features}
+      />
+		)
+	})
     return (
-        <POPOSSpace 
-            id={i}
-            key={title}
-            name={title}
-            address={address}
-            image={images[0]}
-            hours={hours}
-        />
-    )
-})
-  return (
     <div className="POPOSList">
+      <form>
+        <input
+          value={query}
+          placeholder='search'
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
         {spaces}
     </div>
   )
